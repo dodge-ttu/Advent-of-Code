889b6474dedb362b0ac428c6d79a518927c09e1b
Will <dodge.ttu@gmail.com>
2018-12-01 07:30:27 -0600

    add file

--- 2018/day_01.py
+++ 2018/day_01.py
@@ -1,1 +1,24 @@
-# test
+# Terrible hacky solution. Improvements pending
+
+import pandas as pd
+
+df = pd.read_csv("/home/will/advent_of_code/Advent-of-Code/2018/day_01_input.txt", header=None)
+
+values = list(df.loc[:, 0].values) * 100000
+
+def answer(ls):
+    sums = []
+    count = 0
+    sum = 0
+    while True:
+        if (sum + ls[count]) in sums:
+            print(sum + ls[count])
+            break
+        else:
+            print(sum, ls[count])
+            sum += (ls[count])
+            count += 1
+            sums.append(sum)
+
+
+
