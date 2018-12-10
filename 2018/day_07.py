f50c67c6f7340cf41d1408aa3e4d488ddb4f1240
Will <dodge.ttu@gmail.com>
2018-12-09 20:49:44 -0600

    update

--- 2018/day_07.py
+++ 2018/day_07.py
@@ -2,7 +2,7 @@ import timeit
 import re
 
 ####### Problem 1 #######
-
+#
 # The instructions specify a series of steps and requirements about which steps must be finished before others
 # can begin (your puzzle input). Each step is designated by a single letter. For example, suppose you have the
 # following instructions:
@@ -95,7 +95,6 @@ def p1answer2(ls, *args, **kwargs):
 
             else:
                 parsed_instructions.append((one,two))
-
 
         all_pieces = list(dict.fromkeys("".join([i+j for(i,j) in parsed_instructions])))
 
@@ -191,10 +190,57 @@ for (answer_name, answer) in p1answers.items():
 
 ####### Problem 2 #######
 
+# As you're about to begin construction, four of the Elves offer to help. "The sun will set soon; it'll go faster
+# if we work together." Now, you need to account for multiple people working on steps simultaneously. If multiple
+# steps are available, workers should still begin them in alphabetical order.
+#
+# Each step takes 60 seconds plus an amount corresponding to its letter: A=1, B=2, C=3, and so on. So, step A takes
+# 60+1=61 seconds, while step Z takes 60+26=86 seconds. No time is required between steps.
+#
+# To simplify things for the example, however, suppose you only have help from one Elf (a total of two workers) and
+# that each step takes 60 fewer seconds (so that step A takes 1 second and step Z takes 26 seconds). Then, using the
+# same instructions as above, this is how each second would be spent:
+#
+# Second   Worker 1   Worker 2   Done
+#    0        C          .
+#    1        C          .
+#    2        C          .
+#    3        A          F       C
+#    4        B          F       CA
+#    5        B          F       CA
+#    6        D          F       CAB
+#    7        D          F       CAB
+#    8        D          F       CAB
+#    9        D          .       CABF
+#   10        E          .       CABFD
+#   11        E          .       CABFD
+#   12        E          .       CABFD
+#   13        E          .       CABFD
+#   14        E          .       CABFD
+#   15        .          .       CABFDE
+#
+# Each row represents one second of time. The Second column identifies how many seconds have passed as of the
+# beginning of that second. Each worker column shows the step that worker is currently doing (or . if they are idle).
+# The Done column shows completed steps.
+#
+# Note that the order of the steps has changed; this is because steps now take time to finish and multiple workers
+# can begin multiple steps simultaneously.
+#
+# In this example, it would take 15 seconds for two workers to complete these steps.
+
 
 ### Test cases:
 
-p2_a = ([],0)
+p2_a = ([
+    "Step C must be finished before step A can begin.",
+    "Step C must be finished before step F can begin.",
+    "Step A must be finished before step B can begin.",
+    "Step A must be finished before step D can begin.",
+    "Step B must be finished before step E can begin.",
+    "Step D must be finished before step E can begin.",
+    "Step F must be finished before step E can begin.",
+    ], 15)
+
 
 p2_test_cases = {
     "p2_a":p2_a
