87d244943ac8b81bd6ec8a009a0e7a0990bd3bac
dodge-ttu <will@iamdodge.us>
2019-11-30 23:50:10 -0600

    day one done

--- 2019/q01.py
+++ 2019/q01.py
@@ -2,3 +2,37 @@ from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2019, day=1)
 print(puzzle.input_data)
+
+masses = puzzle.input_data.split('\n')
+masses = [int(i) for i in masses]
+
+# Problem 1
+def calc_fuel(x):
+    x = int((x / 3)) - 2
+    return x
+
+required_fuel = []
+for i in masses:
+    required_fuel.append(calc_fuel(i))
+
+p1_answer = sum(required_fuel)
+print(f'[INFO] Problem one solution: {p1_answer}')
+
+# Problem 2
+def calc_fuels_fuel(x):
+    fuels = []
+    while x > 0:
+        x = calc_fuel(x)
+        if x < 0:
+            x = 0
+        fuels.append(x)
+
+    return sum(fuels)
+
+fuels_fuel = []
+for i in masses:
+    fuels_fuel.append(calc_fuels_fuel(i))
+
+p2_answer = sum(fuels_fuel)
+print(f'[INFO] Problem two solution: {p2_answer}')
+
