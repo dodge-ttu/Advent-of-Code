169c4fb5e9ffc6347c50eea3e124d54c504477a2
dodge-ttu <will@iamdodge.us>
2019-12-02 00:46:26 -0600

    ready for day three

--- 2019/q01.py
+++ 2019/q01.py
@@ -6,7 +6,7 @@ data = puzzle.input_data
 masses = data.split('\n')
 masses = [int(i) for i in masses]
 
-# Problem 1
+# Part A
 def calc_fuel(x):
     x = int(x / 3) - 2
     if x < 0:
@@ -18,8 +18,9 @@ for i in masses:
     required_fuel.append(calc_fuel(i))
 
 part_a_answer = sum(required_fuel)
+print(part_a_answer)
 
-# Problem 2
+# Part B
 def calc_fuels_fuel(x):
     fuels = []
     while x > 0:
@@ -32,6 +33,7 @@ for i in masses:
     fuels_fuel.append(calc_fuels_fuel(i))
 
 part_b_answer = sum(fuels_fuel)
+print(part_b_answer)
 
 print(f'[INFO] Puzzle Title: {puzzle.title}')
 print(f'[INFO] Part A answer: {puzzle.answer_a}')
--- 2019/q02.py
+++ 2019/q02.py
@@ -1,3 +1,51 @@
 from aocd.models import Puzzle
+import itertools
 
 puzzle = Puzzle(year=2019, day=2)
+data = puzzle.input_data
+
+opcode = data.split(',')
+opcode = [int(c) for c in opcode]
+
+opcode[1] = 12
+opcode[2] = 2
+
+# Part A
+def read_opcode(opcode):
+    for i in range(0,len(opcode),4):
+        one = opcode[opcode[i+1]]
+        two = opcode[opcode[i+2]]
+        if opcode[i] == 1:
+            new_value = one + two
+            opcode[opcode[i+3]] = new_value
+        elif opcode[i] == 2:
+            new_value = one * two
+            opcode[opcode[i+3]] = new_value
+        elif opcode[i] == 99:
+            break
+
+read_opcode(opcode)
+part_a_answer = opcode[0]
+print(part_a_answer)
+
+# Part B
+opcode = data.split(',')
+opcode = [int(c) for c in opcode]
+
+pairs = itertools.permutations(range(99), 2)
+
+while True:
+    a = opcode.copy()
+    a[1], a[2] = next(pairs)
+    read_opcode(a)
+    if a[0] == 19690720:
+        noun = a[1]
+        verb = a[2]
+        break
+
+part_b_answer = 100 * noun + verb
+print(part_b_answer)
+
+print(f'[INFO] Puzzle Title: {puzzle.title}')
+print(f'[INFO] Part A answer: {puzzle.answer_a}')
+print(f'[INFO] Part B answer: {puzzle.answer_b}')
