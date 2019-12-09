1ea312e189ab66298d10f290b5833d988ac8cc4c
dodge-ttu <will@iamdodge.us>
2019-12-09 13:21:33 -0600

    how not to build an interpreter

--- 2019/q09.py
+++ 2019/q09.py
@@ -6,63 +6,65 @@ data = puzzle.input_data
 intcode = data.split(',')
 intcode = [int(i) for i in intcode]
 
-def read_opcode(opcode, input, ap, rb, grouped_input=False, output=None, extend_mem=10000):
+def read_opcode(opcode, input, ap, rb, output=None, extend_mem=100000):
+    # Extend memory
     ex_mem = [0]*extend_mem
     opcode.extend(ex_mem)
-    input_count = 0
+    # A to variable output indicating a '99'
     finished = False
     while True:
+        # Track whether pointer has been changed by opcode
         ap_mod = False
+        # Format opcode integer for parsing
         pm_inst = str(opcode[ap]).zfill(5)
+        # Get opcode
         opc_info = int(pm_inst[-2:])
+        # Get parameter modes
         fst_param_mode = int(pm_inst[-3])
         scd_param_mode = int(pm_inst[-4])
         thd_param_mode = int(pm_inst[-5])
-        #print(fst_param_mode, scd_param_mode, thd_param_mode)
+        # Grab parameters
         address_1 = opcode[ap + 1]
         address_2 = opcode[ap + 2]
         address_3 = opcode[ap + 3]
+        # Get values for parameters based on mode
         if fst_param_mode == 0:
             val_1 = opcode[address_1]
         elif fst_param_mode == 1:
             val_1 = address_1
         else:
-            val_1 = opcode[address_1 + rb]
+            address_1 += rb
+            val_1 = opcode[address_1]
+
         if scd_param_mode == 0:
             val_2 = opcode[address_2]
         elif scd_param_mode == 1:
             val_2 = address_2
         else:
-            val_2 = opcode[address_2 + rb]
+            address_2 += rb
+            val_2 = opcode[address_2]
+
         if thd_param_mode == 0:
-            val_3 = opcode[address_3]
+            pass
         elif thd_param_mode == 1:
-            val_3 = address_3
+            pass
         else:
-            val_3 = opcode[address_3 + rb]
+            address_3 += rb
+
+        # Perform operations based on code
         if opc_info == 1:
             new_value = val_1 + val_2
-            opcode[val_3] = new_value
+            opcode[address_3] = new_value
             ap += 4
         elif opc_info == 2:
             new_value = val_1 * val_2
-            opcode[val_3] = new_value
+            opcode[address_3] = new_value
             ap += 4
         elif opc_info == 3:
-            if grouped_input:
-                if input_count == 0:
-                    opcode[val_1] = input[0]
-                    input_count += 1
-                else:
-                    opcode[val_1] = input[1]
-            else:
-                opcode[val_1] = input
+            opcode[address_1] = input
             ap += 2
         elif opc_info == 4:
-            if fst_param_mode == 1:
-                output = val_1
-            else:
-                output = opcode[val_1]
+            output = opcode[address_1]
             print(f'output: {output}')
             ap += 2
             return output, opcode, ap, rb, finished
@@ -83,14 +85,14 @@ def read_opcode(opcode, input, ap, rb, grouped_input=False, output=None, extend_
                 store_val = 1
             else:
                 store_val = 0
-            opcode[val_3] = store_val
+            opcode[address_3] = store_val
             ap += 4
         elif opc_info == 8:
             if val_1 == val_2:
                 store_val = 1
             else:
                 store_val = 0
-            opcode[val_3] = store_val
+            opcode[address_3] = store_val
             ap += 4
         elif opc_info == 9:
             rb += val_1
@@ -99,17 +101,14 @@ def read_opcode(opcode, input, ap, rb, grouped_input=False, output=None, extend_
             print('99')
             finished = True
             return output, opcode, ap, rb, finished
-        
-intcode = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
 
-sig, oc, ap, rb, _ = read_opcode(intcode.copy(), input=9, ap=0, rb=0, grouped_input=False)
+#intcode = [1102,34915192,34915192,7,4,7,99,0]
 
-finished = False
+a_sig, oc, ap, rb, _ = read_opcode(intcode.copy(), input=1, ap=0, rb=0)
+a_answer = a_sig
 
-while finished == False:
-    sig, oc, ap, rb, finished = read_opcode(oc, input=1, ap=ap, rb=rb, grouped_input=False)
+b_sig, oc, ap, rb, _ = read_opcode(intcode.copy(), input=2, ap=0, rb=0)
+b_answer = b_sig
 
 # Puzzle metadata
 def time_to_HHMMSS(td):
